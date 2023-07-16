9641eb55af4a0b540a71818fb285d61d9e4ed4dc
sarazayan <57018412+sarazayan@users.noreply.github.com>
2023-07-16 16:06:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ st.set_page_config(page_title='Ask your Doc via PaLM🌴 Model , LangChain 🦜�
 st.title('Ask your Doc via PaLM🌴 Model , LangChain 🦜🔗 and Chroma')
 
 # File upload
-uploaded_file = st.file_uploader('Upload txt file', type='pdf')
+uploaded_file = st.file_uploader('Upload PDF file', type='pdf')
 #pdf= st.file_uploader('Upload PDF file', type='pdf',accept_multiple_files=True)
 
 loader_pdf = PyPDFLoader(uploaded_file)
