35784d89c773cd15ce1d6baac810f23d22b133a6
sarazayan <57018412+sarazayan@users.noreply.github.com>
2023-07-24 11:54:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ from langchain.prompts import PromptTemplate
 from langchain.document_loaders import PyPDFLoader
 from tempfile import NamedTemporaryFile
 
-
 # Build prompt
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
 {context}
