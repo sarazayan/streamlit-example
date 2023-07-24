ac573ab77e4cea95a4579b6867a67bd3e8543e15
sarazayan <57018412+sarazayan@users.noreply.github.com>
2023-07-24 11:03:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,9 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.llms import GooglePalm
 from langchain.embeddings import GooglePalmEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
-from io import StringIO
-from PyPDF2 import PdfReader
 from langchain.document_loaders import PyPDFLoader
+from tempfile import NamedTemporaryFile
 
 
 # Build prompt
@@ -20,55 +18,47 @@ QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 def generate_response(uploaded_file, google_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
-        documents = [uploaded_file.read().decode()]
-        
-
-        
-        
-        # Split documents into chunks
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64, separators=["\n\n", "\n", ",", " ", "."])
-        texts = text_splitter.create_documents(documents)
-        st.write(len(texts))
-        
-        # Select embeddings
-        embeddings = GooglePalmEmbeddings(google_api_key=google_api_key)
-        
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings) 
-        
-        # Create retriever interface
-        retriever = db.as_retriever(k=1)
-        #retriever=db.as_retriever(search_kwargs={"k": 3})
-        # retriever = db.as_retriever(k=2, fetch_k=4)
-        # retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .9})
-        
-        # Create QA chain
-        #qa = RetrievalQA.from_chain_type(llm=GooglePalm(google_api_key=google_api_key, temperature=0.1, max_output_tokens=128), chain_type="stuff", retriever=retriever)
-        qa = RetrievalQA.from_chain_type(llm=GooglePalm(google_api_key=google_api_key, temperature=0.1, max_output_tokens=128),
-                                         chain_type="stuff",
-                                         retriever=retriever,
-                                         return_source_documents=True,chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
-
+        with NamedTemporaryFile(dir='.', suffix='.pdf') as f:
+            f.write(uploaded_file.getbuffer())
+            loader = PyPDFLoader(f.name)
+            pages = loader.load_and_split()
+            st.write("Number of pages=  ", len(pages))
+            
+            # Select embeddings
+            embeddings = GooglePalmEmbeddings(google_api_key=google_api_key)
+            
+            # Create a vectorstore from documents
+            db = Chroma.from_documents(pages, embeddings) 
+            
+            # Create retriever interface
+            retriever = db.as_retriever(k=3)
+            # retriever = db.as_retriever(k=2, fetch_k=4)
+            # retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .9})
+            
+            # Create QA chain
+            #qa = RetrievalQA.from_chain_type(llm=GooglePalm(google_api_key=google_api_key, temperature=0.1, max_output_tokens=128), chain_type="stuff", retriever=retriever)
+            qa = RetrievalQA.from_chain_type(llm=GooglePalm(google_api_key=google_api_key, temperature=0.1, max_output_tokens=128),
+                                             chain_type="stuff",
+                                             retriever=retriever,
+                                             return_source_documents=True,
+                                             chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
     
-                                  
+            try:
+              res = qa({"query": query_text})
+              return res  
+            except:
+              st.write("An error occurred")
 
-    
-        return qa({"query": query_text})
-        #return qa.run(query_text)
+        return
 
 
 # Page title
-st.set_page_config(page_title='Ask your Doc via PaLM🌴 Model , LangChain 🦜🔗 and Chroma')
-st.title('Ask your Doc via PaLM🌴 Model , LangChain 🦜🔗 and Chroma')
+st.set_page_config(page_title='Ask your PDF via PaLM🌴 Model , LangChain 🦜🔗 and Chroma vector DB. By: Ibrahim Sobh')
+st.title('Ask your PDF via PaLM🌴 Model , LangChain 🦜🔗 and Chroma vector DB. By: Ibrahim Sobh')
 
 # File upload
-uploaded_file = st.file_uploader('Upload txt file', type='txt')
-#pdf= st.file_uploader('Upload PDF file', type='pdf',accept_multiple_files=True)
-
-#loader_pdf = PyPDFLoader('2021_02_04_PR_Carbon_Neutrality_objectives.pdf')
-#docs = loader_pdf.load()
-
-
+#uploaded_file = st.file_uploader('Upload text file', type='txt')
+uploaded_file = st.file_uploader('Upload pdf file', type='pdf',accept_multiple_files=True)
 
 # Query text
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
@@ -78,20 +68,16 @@ result = []
 with st.form('myform', clear_on_submit=True):
     google_api_key = st.text_input('Google PaLM🌴 API Key', type='password', disabled=not (uploaded_file and query_text))
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
-    #if submitted and openai_api_key.startswith('AIz'):
     if submitted and google_api_key:
         with st.spinner('Calculating...'):
             response = generate_response(uploaded_file, google_api_key, query_text)
-            st.write(response[0])
-            result.append(response)
+            if response:
+                result.append(response)
             del google_api_key
 
 if len(result):
     st.markdown('**Answer:** **:blue[' + response['result'] + "]**")
     st.markdown('---')
-    
     st.markdown('**References:** ')
     for i, sd in enumerate(response['source_documents']):
-        st.markdown('**Ref ' + str(i) + '** :green[' + sd.page_content[:70] + "... ]")
-        print(sd.metadata)
-        
+        st.markdown('**Page:** :green[' + str(sd.metadata["page"]) + "]")
